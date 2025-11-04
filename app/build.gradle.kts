e66646bb7628cdee57a1ccd181cdeb3e93dd4fda
Your Name <you@example.com>
2025-11-03 20:17:38 -0800

    set minsdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.vayunmathur.pdf"
-        minSdk = 31
+        minSdk = 35
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
