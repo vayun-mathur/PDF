4357cafa9b5ee1e91fb84f8fa006fb79057fbec9
Your Name <you@example.com>
2025-12-26 22:30:05 +0800

    update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
     defaultConfig {
         applicationId = "com.vayunmathur.pdf"
         minSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
